0bceca448f28fd15ab4f23f88dc3abbcf67fb011
Bernhard Winner <bernhard@bernhardwinner@gmail.com>
2022-06-14 02:28:14 +0700

    add function greetings

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/brnhrdwnnr/go-greetings
+
+go 1.18
---
+++ greetings.go
@@ -0,0 +1,9 @@
+package go_greetings
+
+func SayGoodMorning() string {
+	return "Good morning"
+}
+
+func SayHello() string {
+	return "Hello"
+}
